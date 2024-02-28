1389b30d540acb8e0a55bff743df9c8aecf409cd
tim <timothy.j.dailey@gmail.com>
2024-02-28 10:35:57 -0800

    fann example with imgui commented out

--- fann.cpp
+++ fann.cpp
@@ -6,12 +6,13 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <vector>
 
 #include "ulog/ulog.h"
 
 #include "fann/fann.h"
 
-#include "imgui_util.h"
+//#include "imgui_util.h"
 
 using namespace std;
 
@@ -28,13 +29,17 @@ const unsigned int num_output = 10;
 const unsigned int num_layers = 3;
 const unsigned int num_neurons_hidden = 300;
 
+static std::vector<float> x_data_epoch = {  };
+static std::vector<float> y_data_mse = {  };
+
 int FANN_API test_callback(struct fann* ann, struct fann_train_data* train,
     unsigned int max_epochs, unsigned int epochs_between_reports,
     float desired_error, unsigned int epochs)
 {
+    const float mse = fann_get_MSE(ann);
     ofstream outfile;
     outfile.open(error_data_log.c_str(), ios::app);
-    outfile << fann_get_MSE(ann) << endl;
+    outfile << mse << endl;
     outfile.close();
 
     if (epochs % 50 == 0)
@@ -42,13 +47,17 @@ int FANN_API test_callback(struct fann* ann, struct fann_train_data* train,
         std::cout << "Saving Network.... " << std::endl;
         fann_save(ann, network_file.c_str());
     }
-    std::cout << setw(10) << "Epoch: " << setw(10) << epochs << setw(30) << "current mse: " << setw(10) << fann_get_MSE(ann) << std::endl;
+    std::cout << setw(10) << "Epoch: " << setw(10) << epochs << setw(30) << "current mse: " << setw(10) << mse << "size " << x_data_epoch.size() << std::endl;
+
+    x_data_epoch.push_back(epochs);
+    y_data_mse.push_back(mse);
+
     return 0;
 }
 
 void test_it()
 {
-    srand((unsigned)time(0));
+    srand((unsigned)time(0)); 
     int item = (rand() % 55000) + 1;
     fann_type* calc_out;
     fann_type input[28 * 28];
@@ -137,90 +146,104 @@ void test_it()
 
 int main(int argc, char* argv[])
 {
-    Imgui_SetupContext();
+    //Imgui_SetupContext();
 
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    float desired_error = 0.01f;
+    int max_epochs = 500;
+    int epochs_between_reports = 1;
+    struct fann* ann;
+    // Default learning_algorithm
+    fann_train_enum learning_algorithm = FANN_TRAIN_BATCH; 
 
-    // Main loop
-    bool done = false;
-    while (!done)
-    {
-        // Poll and handle messages (inputs, window resize, etc.)
-        // See the WndProc() function below for our to dispatch events to the Win32 backend.
-        MSG msg;
-        while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
-        {
-            ::TranslateMessage(&msg);
-            ::DispatchMessage(&msg);
-            if (msg.message == WM_QUIT)
-                done = true;
-        }
-        if (done)
-            break;
+    //./fann --mse 0.01 --epoch 500 --train training.data --backprop-batch
 
-        // Start the Dear ImGui frame
-        ImGui_ImplDX12_NewFrame();
-        ImGui_ImplWin32_NewFrame();
-        ImGui::NewFrame();
+    std::cout << setw(30) << "Training parameters: "    << setw(100) << ""                  << std::endl;
+    std::cout << setw(30) << "Training File: "          << setw(100) << training_file       << std::endl;
+    std::cout << setw(30) << "Desired error: "          << setw(100) << desired_error       << std::endl;
+    std::cout << setw(30) << "Max epochs: "             << setw(100) << max_epochs          << std::endl;
+    std::cout << setw(30) << "Using network file: "     << setw(100) << network_file        << std::endl;
+    std::cout << setw(30) << "Learning algorithm: "     << setw(100) << learning_algorithm  << std::endl;
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+    ann = fann_create_standard(num_layers, num_input, num_neurons_hidden, num_output);
+    //ann = fann_create_from_file(network_file.c_str());
 
-        int   bar_data[11] = {1,2,3,4,5,6,7,8,9,10,11};
-        float x_data[11] = { 1,2,3,4,5,6,7,8,9,10,11 };
-        float y_data[11] = { 1,2,3,4,5,6,7,8,9,10,11 };
-        std::random_device rd;
-        std::mt19937 g(rd());
-        std::shuffle(std::begin(y_data), std::end(y_data),g);
+    // Activation functions
+    fann_set_activation_function_hidden(ann, FANN_SIGMOID);
+    fann_set_activation_function_output(ann, FANN_SIGMOID);
 
-        
-        if (ImPlot::BeginPlot("My Plot")) {
-            ImPlot::PlotBars("My Bar Plot", bar_data, 11);
-            ImPlot::PlotLine("My Line Plot", x_data, y_data, 11);   
-            ImPlot::EndPlot();
-        }        
+    fann_set_training_algorithm(ann, learning_algorithm);
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-            
-            ImGuiIO& io = ImGui::GetIO(); 
-            (void)io;
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-        Imgui_Render();
+    if (learning_algorithm != FANN_TRAIN_RPROP) {
+        fann_set_learning_momentum(ann, 0.9);
+        fann_set_learning_rate(ann, 1.3);
     }
-    Imgui_Cleanup();
+
+    fann_set_callback(ann, *(test_callback));
+
+    fann_train_on_file(ann, training_file.c_str(), max_epochs, epochs_between_reports, desired_error);
+
+    //Imgui_SetupContext();
+
+    //// Our state
+    //bool show_demo_window = true;
+    //bool show_another_window = false;
+    ////ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    //// Main loop
+    //bool done = false;
+    //while (!done)
+    //{
+    //    // Poll and handle messages (inputs, window resize, etc.)
+    //    // See the WndProc() function below for our to dispatch events to the Win32 backend.
+    //    MSG msg;
+    //    while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
+    //    {
+    //        ::TranslateMessage(&msg);
+    //        ::DispatchMessage(&msg);
+    //        if (msg.message == WM_QUIT)
+    //            done = true;
+    //    }
+    //    if (done)
+    //        break;
+
+    //    // Start the Dear ImGui frame
+    //    ImGui_ImplDX12_NewFrame();
+    //    ImGui_ImplWin32_NewFrame();
+    //    ImGui::NewFrame();
+
+    //    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+    //    if (show_demo_window)
+    //        ImGui::ShowDemoWindow(&show_demo_window);
+
+    //    int   bar_data[11] = {1,2,3,4,5,6,7,8,9,10,11};
+    //    float x_data[11] = { 1,2,3,4,5,6,7,8,9,10,11 };
+    //    float y_data[11] = { 1,2,3,4,5,6,7,8,9,10,11 };
+    //    std::random_device rd;
+    //    std::mt19937 g(rd());
+    //    std::shuffle(std::begin(y_data), std::end(y_data),g);
+
+    //    
+    //    if (ImPlot::BeginPlot("My Plot")) {
+    //        ImPlot::PlotBars("My Bar Plot", bar_data, 11);
+    //        ImPlot::PlotLine("My Line Plot", x_data, y_data, 11);   
+    //        ImPlot::EndPlot();
+    //    }        
+    //    Imgui_Render();
+    //}
+
+    //Imgui_Cleanup();
+
+    std::cout << "Complete. Saving Network.... " << std::endl;
+    fann_save(ann, network_file.c_str());
+
+    fann_destroy(ann);
+
+    //test the newly trained network
+    for (int i = 0; i < 10; i++)
+    {
+        test_it();
+    }
+    exit(0);
 
     return 0;
 }
