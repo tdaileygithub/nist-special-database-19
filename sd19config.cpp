4f09f52678af2df7eec1daccd2a20745a01b71a7
tim <timothy.j.dailey@gmail.com>
2024-03-01 21:05:55 -0800

    update config xml

---
+++ sd19config.cpp
@@ -0,0 +1,45 @@
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "sd19config.h"
+
+std::ostream& operator<<(std::ostream& out, const Sd19Config& config)
+{
+    out << std::endl
+        << std::setw(30) << std::string(30, '-')            << std::setw(85) << std::string(85, '-')                << std::endl
+        << std::setw(30) << "NIST 19 Folder:"               << std::setw(85) << config.GetNistSd19Folder()          << std::endl
+        << std::setw(30) << "Sqlite3_SourceDb:"             << std::setw(85) << config.GetSourceDbName()            << std::endl
+        << std::setw(30) << "Sqlite3_DumpFilename:"         << std::setw(85) << config.GetDumpDbName()              << std::endl
+        << std::setw(30) << "Delete_Existing_Db:"           << std::setw(85) << config.GetDeleteExistingDb()        << std::endl
+        << std::setw(30) << "Hsf_Page_Enable:"              << std::setw(85) << config.GetHfPageProcessing()        << std::endl
+        << std::setw(30) << "Mis_Enable:"                   << std::setw(85) << config.GetMisProcessing()           << std::endl
+        << std::setw(30) << std::string(30, '-')            << std::setw(85) << std::string(85, '-')                << std::endl
+        << std::setw(30) << "Number_Threads:"               << std::setw(85) << config.GetNumberThreads()           << std::endl        
+        << std::setw(30) << std::string(30, '-')            << std::setw(85) << std::string(85, '-')                << std::endl
+        << std::setw(30) << "Training_Algorithm:"           << std::setw(85) << config.GetTraningAlgorithm()        << std::endl
+        << std::setw(30) << "Training_Learning_Momentum:"   << std::setw(85) << config.GetLearningMomemtum()        << std::endl
+        << std::setw(30) << "Training_Learning_Rate:"       << std::setw(85) << config.GetLearningRate()            << std::endl
+        << std::setw(30) << std::string(30, '-')            << std::setw(85) << std::string(85, '-')                << std::endl
+        << std::setw(30) << "Desired_Error:"                << std::setw(85) << config.GetDesiredError()            << std::endl
+        << std::setw(30) << "Max_Epochs:"                   << std::setw(85) << config.GetMaxEpochs()               << std::endl
+        << std::setw(30) << "Epochs_Between_Report:"        << std::setw(85) << config.GetEpochsBetweenReport()     << std::endl
+        << std::setw(30) << std::string(30, '-')            << std::setw(85) << std::string(85, '-')                << std::endl
+        << std::setw(30) << "Action_Function_Hidden:"       << std::setw(85) << config.GetActionFunctionHidden()    << std::endl
+        << std::setw(30) << "Action_Function_Output:"       << std::setw(85) << config.GetActionFunctionOutput()    << std::endl
+        << std::setw(30) << std::string(30, '-')            << std::setw(85) << std::string(85, '-')                << std::endl
+        << std::setw(30) << "Number_Inputs:"                << std::setw(85) << config.GetNumberInputs()            << std::endl
+        << std::setw(30) << "Number_Outputs:"               << std::setw(85) << config.GetNumberOutputs()           << std::endl
+        << std::setw(30) << "Number_Layers:"                << std::setw(85) << config.GetNumberLayers()            << std::endl
+        << std::setw(30) << "Number_Hidden:"                << std::setw(85) << config.GetNumberHidden()            << std::endl
+        << std::setw(30) << std::string(30, '-')            << std::setw(85) << std::string(85, '-')                << std::endl;
+    return out;
+}
+
+std::string to_string(const Sd19Config& config)
+{
+    std::ostringstream ss;
+    ss << config;
+    return ss.str();
+}
--- sd19config.h
+++ sd19config.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <memory>
+#include <mutex>
 #include <string>
+#include <thread>
 
 #include "pugixml/pugixml.hpp"
+
+#include "mnist_fann_extern.h"
+
+#include "fann/fann.h"
+
+#include "sd19db/dbmanager.h"
 
 // a global instance and mutex
 extern std::unique_ptr<sdb19db::DbManager> dbm;
@@ -18,7 +27,22 @@ private:
     bool _hsfPageProcessing = false;
     bool _misProcessing = false;
     bool _deleteExistingDb = false;
-    int  _numberThreads = 1;
+    int _numberThreads = 1;
+    int _numberInputs = 0;
+    int _numberOutputs = 0;
+    int _numberLayers = 0;
+    int _numberHiddenLayers = 0;
+    fann_activationfunc_enum _actionFunctionHidden;
+    std::string _actionFunctionHiddenStr = "";
+    fann_activationfunc_enum _actionFunctionOutput;
+    std::string _actionFunctionOutputStr = "";
+    fann_train_enum _trainingAlgorithm;
+    std::string _trainingAlgorithmStr= "";
+    float _learningMomentum = 0.0f;
+    float _learningRate = 0.0f;
+    float _desiredError = 0.0f;
+    int _maxEpochs = 0;
+    int _epochsBetweenReport = 0;
 
 public:
     Sd19Config() {
@@ -27,13 +51,34 @@ public:
         if (!doc.load_file("config.xml"))
             exit(1);
 
-        _nistSd19Folder     = std::string((pugi::xpath_query("/Sd19Db/Configs/Config[@Name='NIST_Special_Database_19']").evaluate_node_set(doc)[0]).node().text().get());
-        _hsfPageProcessing  = (pugi::xpath_query("/Sd19Db/Configs/Config[@Name='Hsf_Page_Enable']").evaluate_node_set(doc)[0]).node().text().as_bool(true);
-        _misProcessing      = (pugi::xpath_query("/Sd19Db/Configs/Config[@Name='Mis_Enable']").evaluate_node_set(doc)[0]).node().text().as_bool(true);
-        _deleteExistingDb   = (pugi::xpath_query("/Sd19Db/Configs/Config[@Name='Delete_Existing_Db']").evaluate_node_set(doc)[0]).node().text().as_bool(true);
-        _numberThreads      = (pugi::xpath_query("/Sd19Db/Configs/Config[@Name='Number_Threads']").evaluate_node_set(doc)[0]).node().text().as_int(0);
-        _sourceDbFile       = std::string((pugi::xpath_query("/Sd19Db/Configs/Config[@Name='Sqlite3_SourceDb']").evaluate_node_set(doc)[0]).node().text().get());
-        _dbDumpFile         = std::string((pugi::xpath_query("/Sd19Db/Configs/Config[@Name='Sqlite3_DumpFilename']").evaluate_node_set(doc)[0]).node().text().get());
+        _nistSd19Folder     = std::string((pugi::xpath_query("/Sd19/Configs/Config[@Name='NIST_Special_Database_19']").evaluate_node_set(doc)[0]).node().text().get());
+        _hsfPageProcessing  = (pugi::xpath_query("/Sd19/Configs/Config[@Name='Hsf_Page_Enable']").evaluate_node_set(doc)[0]).node().text().as_bool(true);
+        _misProcessing      = (pugi::xpath_query("/Sd19/Configs/Config[@Name='Mis_Enable']").evaluate_node_set(doc)[0]).node().text().as_bool(true);
+        _deleteExistingDb   = (pugi::xpath_query("/Sd19/Configs/Config[@Name='Delete_Existing_Db']").evaluate_node_set(doc)[0]).node().text().as_bool(true);
+        _numberThreads      = (pugi::xpath_query("/Sd19/Configs/Config[@Name='Number_Threads']").evaluate_node_set(doc)[0]).node().text().as_int(0);
+        _sourceDbFile       = std::string((pugi::xpath_query("/Sd19/Configs/Config[@Name='Sqlite3_SourceDb']").evaluate_node_set(doc)[0]).node().text().get());
+        _dbDumpFile         = std::string((pugi::xpath_query("/Sd19/Configs/Config[@Name='Sqlite3_DumpFilename']").evaluate_node_set(doc)[0]).node().text().get());
+
+        _numberInputs       = (pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Number_Inputs']").evaluate_node_set(doc)[0]).node().text().as_int(0);
+        _numberOutputs      = (pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Number_Outputs']").evaluate_node_set(doc)[0]).node().text().as_int(0);
+        _numberLayers       = (pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Number_Layers']").evaluate_node_set(doc)[0]).node().text().as_int(0);
+        _numberHiddenLayers = (pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Number_Hidden']").evaluate_node_set(doc)[0]).node().text().as_int(0);
+
+        _actionFunctionHiddenStr    = std::string((pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Action_Function_Hidden']").evaluate_node_set(doc)[0]).node().text().get());
+        _actionFunctionHidden       = fann_activation_func_to_enum[_actionFunctionHiddenStr];
+
+        _actionFunctionOutputStr    = std::string((pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Action_Function_Output']").evaluate_node_set(doc)[0]).node().text().get());
+        _actionFunctionOutput       = fann_activation_func_to_enum[_actionFunctionOutputStr];
+
+        _trainingAlgorithmStr       = std::string((pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Training_Algorithm']").evaluate_node_set(doc)[0]).node().text().get());
+        _trainingAlgorithm          = fann_training_algo_to_enum[_trainingAlgorithmStr];
+
+        _learningMomentum           = (pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Training_Learning_Momentum']").evaluate_node_set(doc)[0]).node().text().as_float(0);
+        _learningRate               = (pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Training_Learning_Rate']").evaluate_node_set(doc)[0]).node().text().as_float(0);
+
+        _desiredError               = (pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Desired_Error']").evaluate_node_set(doc)[0]).node().text().as_float(0);
+        _maxEpochs                  = (pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Max_Epochs']").evaluate_node_set(doc)[0]).node().text().as_int(0);
+        _epochsBetweenReport        = (pugi::xpath_query("/Sd19/NeuralNetConfig/Config[@Name='Epochs_Between_Report']").evaluate_node_set(doc)[0]).node().text().as_int(0);        
     }
 
     std::string GetSourceDbName() const {
@@ -61,4 +106,57 @@ public:
         }
         return _numberThreads;
     }
+
+    int GetNumberInputs() const {
+        return _numberInputs;
+    }
+    int GetNumberOutputs() const {
+        return _numberOutputs;
+    }
+    int GetNumberHidden() const {
+        return _numberHiddenLayers;
+    }
+    int GetNumberLayers() const {
+        return _numberLayers;
+    }
+
+    fann_activationfunc_enum GetActionFunctionHiddenEnum() const {
+        return _actionFunctionHidden;
+    }
+    std::string GetActionFunctionHidden() const {
+        return _actionFunctionHiddenStr;
+    }
+    fann_activationfunc_enum GetActionFunctionOutputEnum() const {
+        return _actionFunctionOutput;
+    }
+    std::string GetActionFunctionOutput() const {
+        return _actionFunctionOutputStr;
+    }
+    std::string GetTraningAlgorithm() const {
+        return _trainingAlgorithmStr;
+    }
+    fann_train_enum GetTraningAlgorithmEnum() const {
+        return _trainingAlgorithm;
+    }
+
+    float GetLearningMomemtum() const {
+        return _learningMomentum;
+    }
+    float GetLearningRate() const {
+        return _learningRate;
+    } 
+
+    float GetDesiredError() const {
+        return _desiredError;
+    }
+    int GetMaxEpochs() const {
+        return _maxEpochs;
+    }
+    int GetEpochsBetweenReport() const {
+        return _epochsBetweenReport;
+    }
 };
+
+std::ostream& operator<<(std::ostream& out, const Sd19Config& config);
+
+std::string to_string(const Sd19Config& config);
