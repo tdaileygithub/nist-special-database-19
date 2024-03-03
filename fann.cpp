59ec2a13377e12988910ee617a68860537c98ead
tim <timothy.j.dailey@gmail.com>
2024-03-02 21:28:53 -0800

    fann - print config and pass into setup function

--- fann.cpp
+++ fann.cpp
@@ -40,6 +40,9 @@ static float max_mse = 0;
 static unsigned int epoch = 0;
 static float mse = 0.0f;
 
+
+static std::chrono::steady_clock::time_point beginepoch = std::chrono::steady_clock::now();
+
 int FANN_API test_callback(struct fann* ann, struct fann_train_data* train,
     unsigned int max_epochs, unsigned int epochs_between_reports,
     float desired_error, unsigned int epochs)
@@ -55,10 +58,17 @@ int FANN_API test_callback(struct fann* ann, struct fann_train_data* train,
         std::cout << "Saving Network.... " << std::endl;
         fann_save(ann, network_file.c_str());
     }
+    std::chrono::steady_clock::time_point endepoch = std::chrono::steady_clock::now();
     std::cout << setw(10)   << "Epoch: "        << setw(10) << epochs       << setw(30) 
+                            << "elpased : "     << setw(10) << std::chrono::duration_cast<std::chrono::milliseconds>(endepoch - beginepoch).count() << setw(30)
                             << "current mse: "  << setw(10) << mse          << setw(30)
                             << "max mse "       << max_mse                  << setw(30) << std::endl;
     
+    beginepoch = std::chrono::steady_clock::now();
+    //std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    //std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+
+
     x_data_epoch_mse_sb.AddPoint((float)epochs, mse);
 
     return 0;
@@ -186,26 +196,12 @@ void test_it()
 
 void train_thread(fann* ann, const Sd19Config& config)
 {
-    int num_data = 651375;
-    
-    num_data = 2^18 -1;
-    num_data = 262000;
+    int num_data = train_dataset.size();
 
-    num_data = train_dataset.size();
+    const size_t num_bytes = static_cast<size_t>(32 * 32) * num_data;
 
-    const size_t num_bytes = static_cast<size_t>(128 * 128) * num_data;
-
-    std::cerr << "num_data: " << num_data << std::endl;
-    std::cerr << "size bytes num_data: " << num_bytes * sizeof(float) << std::endl;
-
-    //exit(99);
-
-    //251000 okay
-    //260000 okay
-    
-    //351000 crash
-    //300000
-    //275000
+    std::cerr << "num_data:   " << num_data << std::endl;
+    std::cerr << "size in MB: " << (num_bytes * sizeof(float)) / (1000 * 1000)  << std::endl;
 
     fann_train_data* data = fann_create_train_from_callback(num_data, config.GetNumberInputs(), config.GetNumberOutputs(), fann_get_train_data);
 
@@ -214,120 +210,10 @@ void train_thread(fann* ann, const Sd19Config& config)
 
 int main(int argc, char* argv[])
 {
-#if 0
-    fann_type* data_input, * data_output;
-    unsigned int i;
-    struct fann_train_data* data = (struct fann_train_data*)malloc(sizeof(struct fann_train_data));
-
-    if (data == NULL) {
-        fann_error(NULL, FANN_E_CANT_ALLOCATE_MEM);
-        return NULL;
-    }
-
-    fann_init_error_data((struct fann_error*)data);
-
-    const unsigned int num_data = 330908;
-    const unsigned int num_input = 128 * 128;
-    const unsigned int num_output = 26 + 26 + 10;;
-    const size_t num_bytes = static_cast<size_t>(num_input) * num_data;
-
-    data->num_data = num_data;
-    data->num_input = num_input;
-    data->num_output = num_output;
-    data->input = (fann_type**)calloc(num_data, sizeof(fann_type*));
-
-    memset(data->input, 0xFF, num_data * 4);
-
-    if (data->input == NULL) {
-        fann_error(NULL, FANN_E_CANT_ALLOCATE_MEM);
-        fann_destroy_train(data);
-        return NULL;
-    }
-
-    data->output = (fann_type**)calloc(num_data, sizeof(fann_type*));
-    if (data->output == NULL) {
-        fann_error(NULL, FANN_E_CANT_ALLOCATE_MEM);
-        fann_destroy_train(data);
-        return NULL;
-    }
-    
-    data_input = (fann_type *)calloc(num_bytes, sizeof(fann_type));
-    if (data_input == NULL) {
-        fann_error(NULL, FANN_E_CANT_ALLOCATE_MEM);
-        fann_destroy_train(data);
-        return NULL;
-    }
-
-    data_output = (fann_type*)calloc(num_output * num_data, sizeof(fann_type));
-    if (data_output == NULL) {
-        fann_error(NULL, FANN_E_CANT_ALLOCATE_MEM);
-        fann_destroy_train(data);
-        return NULL;
-    }
-
-    for (i = 0; i != num_data; i++) {
-        //set each picture to point at the offset into the large array
-        data->input[i] = data_input;
-
-        std::fill(data->input[i], data->input[i] + num_input, (float)i );
-
-        //advance by one picture 128x128
-        data_input += num_input;
-
-        data->output[i] = data_output;
-        data_output += num_output;
-        //if (i == 275057 || i == 275056)
-        {
-            //printf("data_input:  %d %p \n", i, data_input);
-        }
-        
-    
-    }
-
-
-    printf("\n\ndata_input:       %p \n", data_input);
-    printf("data_input[n-1]:  %p \n\n", data->input[num_data - 1]);
-
-    printf("data_output:      %p \n",  data_output);
-    printf("data_output[n-1]: %p \n\n", data->output[num_data - 1]);
-
-
-    //68765
-    //float *input = data->input[20];
-
-    for (int z = 0; z != num_data; z++) 
-    {
-        if (z == 275057 || z == 275056)
-        {
-            printf("data_input:  %d %p \n", z, data->input[z]);
-        }
-
-
-        float* input = data->input[z];
-
-        
-        int ct = 0;
-        for (int i = 0; i < 128; i++)
-        {
-            for (int j = 0; j < 128; j++)
-            {
-                const int offset = (128 * i) + j;
-                input[ct] = (float)z;
-            }
-            ct++;
-        }
-    }
-
-    exit(99);
-#endif
-
     Sd19Config config;
     std::cout << config;
 
-    //exit(999);
     dbm = std::make_unique< sdb19db::DbManager>("sd19.db3");
-    //auto rai = dbm->GetImageAsPng(67875, "mis", "image");
-    //std::cout << rai;
     
     constexpr float trainsplit = 0.8;
 
