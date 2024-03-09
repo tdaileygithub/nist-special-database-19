a2560e01e3bd6974f219fe9448e3c80eb6d2d88a
tim <timothy.j.dailey@gmail.com>
2024-03-08 22:42:18 -0800

    fann update

--- fann.cpp
+++ fann.cpp
@@ -26,47 +26,44 @@
 using namespace std;
 
 /* global variables definitions */
-string error_data_log   = "error_data.dat";
-string network_file     = "network.nn";
-string training_file    = "training.data";
-string images_file      = "train-images-idx3-ubyte";
-string labels_file      = "train-labels-idx1-ubyte";
 
-// global instance - only want one owner and wrapped with mutex
 std::unique_ptr<sdb19db::DbManager> dbm = nullptr;
+std::unique_ptr<Sd19Config> config = nullptr;
 
-static unsigned int epoch = 0;
-static float mse = 0.0f;
+int FANN_API test_callback(
+    struct fann* ann, 
+    struct fann_train_data* train,
+    unsigned int max_epochs, 
+    unsigned int epochs_between_reports,
+    float desired_error, 
+    unsigned int epochs)
+{    
+    const float mse = fann_get_MSE(ann);
 
-static std::chrono::steady_clock::time_point beginepoch = std::chrono::steady_clock::now();
-
-int FANN_API test_callback(struct fann* ann, struct fann_train_data* train,
-    unsigned int max_epochs, unsigned int epochs_between_reports,
-    float desired_error, unsigned int epochs)
-{
-    epoch = epochs;
-    mse = fann_get_MSE(ann);
-
-    if (epochs % 10 == 0)
+    if (0 == (epochs % config->GetEpochsBetweenReport()))
     {
         std::cout << "Saving Network.... " << std::endl;
-        fann_save(ann, network_file.c_str());
-    }
-    std::chrono::steady_clock::time_point endepoch = std::chrono::steady_clock::now();
-    std::cout << setw(10)   << "Epoch: "        << setw(10) << epochs       << setw(30) 
-                            << "current mse: "  << setw(10) << mse          << setw(30) << std::endl;
+        fann_save(ann, config->GetNetworkFilename().c_str());
 
+        std::cout << setw(10)   << "Epoch: "        << setw(10) << epochs       << setw(30) 
+                                << "current mse: "  << setw(10) << mse          << setw(30) << std::endl;
+    }
     return 0;
 }
 
-void fann_get_train_data(unsigned int num, unsigned int num_input, unsigned int num_output, fann_type* input, fann_type* output)
+void fann_get_train_data(
+    unsigned int num, 
+    unsigned int num_input, 
+    unsigned int num_output, 
+    fann_type* input, 
+    fann_type* output)
 {
     const auto t        = train_dataset.at(num);
     const char label    = std::get<0>(t);
     const int id        = std::get<1>(t);
     const int label_idx = label_to_index[label];
 
-    auto rai = dbm->GetImageAsPng(id, "mis", "image");
+    const auto rai = dbm->GetImageAsPng(id, "mis", "image");
 
     //std::cout << rai << std::endl;
 
@@ -76,82 +73,47 @@ void fann_get_train_data(unsigned int num, unsigned int num_input, unsigned int 
         for (int j = 0; j < rai.Width; j++)
         {
             const int offset = (rai.Width * i) + j;
-            input[ct] = (float)(rai.PixelData[offset]) / 255.0f;
+            input[ct] = (rai.PixelData[offset]) ? (float)MIS_FOREGROUND_PIXEL : (float)MIS_BACKGROUND_PIXEL;
         }
         ct++;
-    }       
-    for (int i = 0; i < num_output; i++)
-    {
-        if (i == label_idx) {
-            output[i] = 0.9f;
-        }
-        else {
-            output[i] = 0.1f;
-        }        
     }
+    for (int i = 0; i < num_output; i++) {
+        output[i] = 0.0f;
+    }
+    output[label_idx] = 1.0f;
     free(rai.PixelData);
 }
 
-void test_it()
+void test_it(const int idx)
 {
-    srand((unsigned)time(0)); 
-    int item = (rand() % 55000) + 1;
+    const auto testchar = test_dataset.at(idx);
+    const char labelChar = std::get<0>(testchar);
+    const int labelIndex = label_to_index[labelChar];
+
+    const auto rai = dbm->GetImageAsPng(std::get<1>(testchar), "mis", "image");
+    //std::cout << rai;
+
     fann_type* calc_out;
-    fann_type input[28 * 28];
+    fann_type input[32 * 32];
     struct fann* ann;
-    FILE* img_file;
-    FILE* lbl_file;
 
-    ann = fann_create_from_file(network_file.c_str());
+    ann = fann_create_from_file(config->GetNetworkFilename().c_str());
 
-    if (!(img_file = fopen(images_file.c_str(), "rb")))
+    for (int i = 0; i < rai.Height; i++)
     {
-        exit(1);
+        for (int j = 0; j < rai.Width; j++)
+        {
+            int offset = rai.Width * i + j;
+            input[offset] = (rai.PixelData[offset]) ? (float)MIS_FOREGROUND_PIXEL : (float)MIS_BACKGROUND_PIXEL;
+        }
     }
-    if (!(lbl_file = fopen(labels_file.c_str(), "rb")))
-    {
-        exit(1);
-    }
-
-    fseek(img_file, 16, SEEK_SET);
-    fseek(img_file, 28 * 28 * item, SEEK_CUR);
-
-    fseek(lbl_file, 8, SEEK_SET);
-    fseek(lbl_file, item, SEEK_CUR);
-
-    unsigned char* lbl = new unsigned char;
-    unsigned char* img = new unsigned char[28 * 28];
-    fread(img, 1, 28 * 28, img_file);
-    fread(lbl, 1, 1, lbl_file);
-
-    int pixel;
-    int label = *lbl;
-
-    std::cout << "+----------------------------+" << std::endl;
-    for (int i = 0; i < 28 * 28; i++)
-    {
-        pixel = img[i];
-        input[i] = pixel;
-
-        if (i % 28 == 0)
-            std::cout << "|";
-        if (pixel > 0)
-            if (pixel > 127)
-                std::cout << "X";
-            else
-                std::cout << ".";
-        else
-            std::cout << " ";
-        if (i % 28 == 27)
-            std::cout << "|" << std::endl;
-    }
-    std::cout << "+----------------------------+" << std::endl;
 
     calc_out = fann_run(ann, input);
 
     float out[2] = { -1, -1000 };
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 62; i++)
     {
+        //find max probability by class
         if (out[1] <= calc_out[i])
         {
             out[0] = i;
@@ -161,54 +123,49 @@ void test_it()
     }
     std::cout << std::endl << std::endl;
 
-    if (out[0] == label)
+    if (out[0] == labelIndex)
         std::cout << "  *** CORRECT ***";
     else
         std::cout << "  --- WRONG ---";
+
     std::cout << std::endl << std::endl;
 
     std::cout << 
-        "Example number "   << item << std::endl <<
-        "Expected  : "      << label << std::endl <<
-        "Calculated: "      << out[0] << std::endl;
+        setw(20) << "MIS Id: "              << setw(10) << idx                          << std::endl <<
+        setw(20) << "Expected Char:"        << setw(10) << labelChar                    << std::endl <<
+        setw(20) << "Expected Index:"       << setw(10) << labelIndex                   << std::endl <<
+        setw(20) << "Calculated Char: "     << setw(10) << label_to_index[(int)out[0]]  << std::endl <<
+        setw(20) << "Calculated Index: "    << setw(10) << (int)out[0]                  << std::endl;
 
     fann_destroy(ann);
-
-    delete[] img;
-    delete lbl;
-
-    fclose(img_file);
-    fclose(lbl_file);
 }
 
-void train_thread(fann* ann, const Sd19Config& config)
+void train_thread(fann* ann, const Sd19Config* config)
 {
-    const unsigned int num_data = train_dataset.size();
-    const unsigned int img_dim = config.GetMisScale() ? (32 * 32) : (128 * 128);
+    const unsigned int num_data = train_dataset.size() + test_dataset.size();
+    const unsigned int img_dim = config->GetMisScale() ? (32 * 32) : (128 * 128);
     const size_t num_bytes = static_cast<size_t>(img_dim) * num_data * sizeof(float);
 
     std::cerr << "num_data:   " << num_data << std::endl;
     std::cerr << "size in MB: " << (num_bytes) / (1000 * 1000)  << std::endl;
 
-    fann_train_data* data = fann_create_train_from_callback(num_data, (unsigned int )config.GetNumberInputs(), (unsigned int)config.GetNumberOutputs(), fann_get_train_data);
+    fann_train_data* data = fann_create_train_from_callback(train_dataset.size(), (unsigned int )config->GetNumberInputs(), (unsigned int)config->GetNumberOutputs(), fann_get_train_data);
 
-    fann_train_on_data(ann, data, config.GetMaxEpochs(), config.GetEpochsBetweenReport(), config.GetDesiredError());
+    fann_train_on_data(ann, data, config->GetMaxEpochs(), config->GetEpochsBetweenReport(), config->GetDesiredError());
 }
 
 int main(int argc, char* argv[])
 {
-    Sd19Config config;
-    std::cout << config << std::endl << std::endl;
+    std::random_device rd;
+    std::mt19937 rng(rd());
 
-    if (!std::filesystem::exists(config.GetSourceDbName())) {
-        std::cerr << config.GetSourceDbName() << " not found" << std::endl;
-        exit(1);
-    }
-
-    dbm = std::make_unique< sdb19db::DbManager>(config.GetSourceDbName());
+    config = std::make_unique< Sd19Config>();
+    std::cout << *config << std::endl << std::endl;
     
-    const float trainsplit = (config.GetTrainTestSplitPercent() / 100.0f);
-    const float testsplit =  1.0f - (config.GetTrainTestSplitPercent() / 100.0f);
+    dbm = std::make_unique< sdb19db::DbManager>(config->GetSourceDbName());
+    
+    const float trainsplit = (config->GetTrainTestSplitPercent() / 100.0f);
+    const float testsplit =  1.0f - (config->GetTrainTestSplitPercent() / 100.0f);
 
     //Stratified training dataset    
     for (int asciichar = 48; asciichar <= 122; asciichar++)
@@ -248,54 +205,64 @@ int main(int argc, char* argv[])
             test_dataset.push_back(std::make_tuple((char)asciichar, *testid));
         }
     }
-    //shuffle the test + train vectors to avoid small sets only capturing from first few hsf pages
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(train_dataset.begin(), train_dataset.end(), g);
-    std::shuffle(test_dataset.begin(),  test_dataset.end(), g);
+    //shuffle the test + train vectors to avoid small sets only capturing from first few hsf pages or centrailized on a single char    
+    std::shuffle(train_dataset.begin(), train_dataset.end(), rng);
+    std::shuffle(test_dataset.begin(),  test_dataset.end(), rng);
 
-    //prune the train and test to have at most config.GetMaxDatasetSize() entries
+    //train_dataset and test_dataset are already in the GetTrainTestSplitPercent() proportions
+
+    //prune the train and test to have at most config->GetMaxDatasetSize() entries
     const int numTrainSplit = (int)(trainsplit * train_dataset.size());
-    const int numTestSplit = (int)(testsplit * test_dataset.size());
-    if (numTrainSplit > config.GetMaxDatasetSize()) {
-        train_dataset.erase(train_dataset.begin() + numTrainSplit, train_dataset.end());
+    const int numTestSplit  = (int)(testsplit * test_dataset.size());
+    const int numTotalDataSet = train_dataset.size() + test_dataset.size();
+
+    if (numTotalDataSet > config->GetMaxDatasetSize())
+    {        
+        const int numDatasetToPrune = numTotalDataSet - config->GetMaxDatasetSize();
+        const int numTrainSetToPrune = trainsplit * numDatasetToPrune;
+        const int numTestSetToPrune = testsplit * numDatasetToPrune;
+
+        train_dataset.erase(train_dataset.cbegin(), train_dataset.cbegin() + numTrainSetToPrune);
+        test_dataset.erase(test_dataset.cbegin(), test_dataset.cbegin() + numTestSetToPrune);
     }
-    if (numTestSplit > config.GetMaxDatasetSize()) {
-        test_dataset.erase(test_dataset.begin() + numTestSplit, train_dataset.end());
-    }
 
-    std::cout << "GetMaxDatasetSize:" << config.GetMaxDatasetSize() << std::endl;
-    std::cout << "Training Entries: " << train_dataset.size() << std::endl;
-    std::cout << "Test Entries:     " << test_dataset.size() << std::endl;
+    std::cout << std::endl
+        << std::setw(30) << std::string(30, '-')    << std::setw(85)    << std::string(85, '-')                         << std::endl
+        << std::setw(30) << "GetMaxDatasetSize:"    << std::setw(85)    << config->GetMaxDatasetSize()                  << std::endl
+        << std::setw(30) << "# Training MIS:"       << std::setw(85)    << train_dataset.size()                         << std::endl
+        << std::setw(30) << "# Test MIS:"           << std::setw(85)    << test_dataset.size()                          << std::endl
+        << std::setw(30) << "Total MIS:"            << std::setw(85)    << train_dataset.size() + test_dataset.size()   << std::endl
+        << std::setw(30) << std::string(30, '-')    << std::setw(85)    << std::string(85, '-')                         << std::endl;
 
-    //ann = fann_create_from_file(network_file.c_str());
-    struct fann* ann = fann_create_standard(config.GetNumberLayers(), config.GetNumberInputs(), config.GetNumberHidden(), config.GetNumberOutputs());
+    struct fann* ann = fann_create_standard(config->GetNumberLayers(), config->GetNumberInputs(), config->GetNumberHidden(), config->GetNumberOutputs());
 
-    // Activation functions
-    fann_set_activation_function_hidden(ann, config.GetActionFunctionHiddenEnum());
-    fann_set_activation_function_output(ann, config.GetActionFunctionOutputEnum());
+    fann_set_activation_function_hidden(ann, config->GetActionFunctionHiddenEnum());
+    fann_set_activation_function_output(ann, config->GetActionFunctionOutputEnum());
 
-    fann_set_training_algorithm(ann, config.GetTraningAlgorithmEnum());
+    fann_set_training_algorithm(ann, config->GetTraningAlgorithmEnum());
 
-    if (config.GetTraningAlgorithmEnum() != FANN_TRAIN_RPROP) {
-        fann_set_learning_momentum(ann, config.GetLearningMomemtum());
-        fann_set_learning_rate(ann, config.GetLearningRate());
+    if (config->GetTraningAlgorithmEnum() != FANN_TRAIN_RPROP) {
+        fann_set_learning_momentum(ann, config->GetLearningMomemtum());
+        fann_set_learning_rate(ann, config->GetLearningRate());
     }
 
     fann_set_callback(ann, *(test_callback));
 
-    thread thread_fann(train_thread, ann, config );
+    thread thread_fann(train_thread, ann, config.get() );
     thread_fann.join();
 
     std::cout << "Complete. Saving Network.... " << std::endl;
-    fann_save(ann, network_file.c_str());
+    fann_save(ann, config->GetNetworkFilename().c_str());
 
     fann_destroy(ann);
+
+    std::uniform_int_distribution<std::mt19937::result_type> testdist(0, test_dataset.size() - 1);
 
     //test the newly trained network
     for (int i = 0; i < 10; i++)
     {
-        test_it();
+        const int idx = testdist(rng);
+        test_it(idx);
     }
     return 0;
 }
