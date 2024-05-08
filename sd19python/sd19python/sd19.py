d8a86d69c9166d5f9e8c282a0c4a68cd7903f816
tim dailey <timothy.j.dailey@gmail.com>
2024-05-08 10:10:05 -0700

    add optimzer with save

--- sd19python/sd19python/sd19.py
+++ sd19python/sd19python/sd19.py
@@ -14,7 +14,6 @@ from Sd19Dataset import Sd19Dataset
 from Sd19TrainDataset import Sd19TrainDataset
 from Sd19TestDataset import Sd19TestDataset
 from Sd19Net import Sd19Net
-   
 
 def train(model, device, train_loader, optimizer, epoch):
     model.train()
@@ -26,12 +25,12 @@ def train(model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
+        optimizer.zero_grad() 
+
         if batch_idx % 50 == 0:
-            torch.save(model.state_dict(), "mnist_cnn.pt")
             print('Train Epoch: {} Batch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-
 
 def test(model, device, test_loader):
     model.eval()
@@ -58,8 +57,8 @@ if __name__ == "__main__":
     dataset_train = Sd19TrainDataset(db_file="../../out/build/x64-release/sd19.db3")
     dataset_test = Sd19TestDataset(db_file="../../out/build/x64-release/sd19.db3")
     
-    train_loader = torch.utils.data.DataLoader(dataset_train,sampler=None, batch_size=1500, batch_sampler=None, num_workers=0)
-    test_loader = torch.utils.data.DataLoader(dataset_test,sampler=None, batch_size=1500, batch_sampler=None, num_workers=0)
+    train_loader = torch.utils.data.DataLoader(dataset_train,sampler=None, batch_size=750, batch_sampler=None, num_workers=0)
+    test_loader = torch.utils.data.DataLoader(dataset_test,sampler=None, batch_size=750, batch_sampler=None, num_workers=0)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using device:', device)
@@ -67,7 +66,10 @@ if __name__ == "__main__":
     model = Sd19Net().to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=1.0)
     scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
-    for epoch in range(1, 14 + 1):
+
+    for epoch in range(1, 10):
         train(model, device, train_loader, optimizer, epoch)
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+        
         test(model, device, test_loader)
         scheduler.step()
